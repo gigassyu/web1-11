7051eb562bf647be0d5e894a2d58399392882436
gigassyu <dragonquest.3710@ezweb.ne.jp>
2017-06-19 17:17:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,16 @@
-window.alert('こんにちは世界！');
-console.log('こんにちは世界！');
+
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
+
+var message;
+if(answer === number) {
+  message ='あたり';
+}eise if (answer < number){
+  message = 'ざんねんもっとおおきいです';
+}eise if (number < answer){
+  message = 'ざんねんもっとちいさいです';
+}eise{
+  message = '0～3の数字を入力してください';
+}
+
+document.getElementById('coice').textContent = message;
